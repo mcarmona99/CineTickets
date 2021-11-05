b771ae0ff78a05f17bfba1714ec4c3f15d353a54
Manuel Carmona Pérez <e.manuelvillar@go.ugr.es>
2021-11-05 01:51:53 +0100

    Añadida clase de excepcion de CineTickets y sus tests, close #29

---
+++ exceptions/tests/test_CineTicketsException.py
@@ -0,0 +1,24 @@
+from exceptions.CineTicketsException import CineTicketsException
+from assertpy import *
+
+def test_CineTicketsException():
+    """Prueba que los objetos de la clase CineTicketsException son creados de manera satisfactoria."""
+    # Arrange and Act
+    CODIGO_TEST = 1
+    ct_exception = CineTicketsException(CODIGO_TEST)
+
+    # Assert
+    assert_that(ct_exception).is_type_of(CineTicketsException)
+    assert_that(ct_exception).is_instance_of(Exception)
+    assert_that(ct_exception.codigo).is_equal_to(CODIGO_TEST)
+    assert_that(ct_exception.mensaje).is_equal_to(CineTicketsException.ERROR_CODES_DESCRIPTION[CODIGO_TEST])
+
+    # Arrange and Act
+    CODIGO_TEST_NO_EXISTENTE = -99999
+    ct_exception = CineTicketsException(CODIGO_TEST_NO_EXISTENTE)
+
+    # Assert
+    assert_that(ct_exception).is_type_of(CineTicketsException)
+    assert_that(ct_exception).is_instance_of(Exception)
+    assert_that(ct_exception.codigo).is_equal_to(CODIGO_TEST_NO_EXISTENTE)
+    assert_that(ct_exception.mensaje).is_equal_to(CineTicketsException.MENSAJE_ERROR_NO_EXISTENTE)

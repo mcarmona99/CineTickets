7e8853cf66c26cf553de3a96b2017de895e66689
Manuel Carmona Pérez <e.manuelvillar@go.ugr.es>
2021-11-12 21:08:37 +0100

    Corregidos tests tras los cambios en excepciones

--- tests/test_CineTicketsException.py
+++ tests/test_CineTicketsException.py
@@ -1,24 +1,52 @@
-from cinetickets.CineTicketsException import CineTicketsException
+import pytest
 from assertpy import *
 
-def test_CineTicketsException():
-    """Prueba que los objetos de la clase CineTicketsException son creados de manera satisfactoria."""
-    # Arrange and Act
-    CODIGO_TEST = 1
-    ct_exception = CineTicketsException(CODIGO_TEST)
+from cinetickets.CineTicketsException import CineTicketsClientError, CineTicketsServerError, CineTicketsNotFound
+
+
+def test_CineTicketsClientError():
+    """Prueba que los objetos de la clase CineTicketsClientError son creados de manera satisfactoria."""
+    # Arrange
+    msg = 'Ejemplo'
+
+    # Act
+    ct_exception = CineTicketsClientError(msg)
 
     # Assert
-    assert_that(ct_exception).is_type_of(CineTicketsException)
+    assert_that(ct_exception).is_type_of(CineTicketsClientError)
     assert_that(ct_exception).is_instance_of(Exception)
-    assert_that(ct_exception.codigo).is_equal_to(CODIGO_TEST)
-    assert_that(ct_exception.mensaje).is_equal_to(CineTicketsException.ERROR_CODES_DESCRIPTION[CODIGO_TEST])
+    assert_that(ct_exception.args[0]).is_equal_to(f'CLIENT ERROR: {msg}')
 
-    # Arrange and Act
-    CODIGO_TEST_NO_EXISTENTE = -99999
-    ct_exception = CineTicketsException(CODIGO_TEST_NO_EXISTENTE)
+
+def test_CineTicketsServerError():
+    """Prueba que los objetos de la clase CineTicketsServerError son creados de manera satisfactoria."""
+    # Arrange
+    msg = 'Ejemplo'
+
+    # Act
+    ct_exception = CineTicketsServerError(msg)
 
     # Assert
-    assert_that(ct_exception).is_type_of(CineTicketsException)
+    assert_that(ct_exception).is_type_of(CineTicketsServerError)
     assert_that(ct_exception).is_instance_of(Exception)
-    assert_that(ct_exception.codigo).is_equal_to(CODIGO_TEST_NO_EXISTENTE)
-    assert_that(ct_exception.mensaje).is_equal_to(CineTicketsException.MENSAJE_ERROR_NO_EXISTENTE)
+    assert_that(ct_exception.args[0]).is_equal_to(f'SERVER ERROR: {msg}')
+
+
+class RecursoDummy:
+    def __init__(self):
+        self.var1 = 'variable1'
+        self.var2 = 'variable2'
+
+
+@pytest.mark.parametrize('argumento', [RecursoDummy(), 'Ejemplo'])
+def test_CineTicketsNotFound(argumento):
+    """Prueba que los objetos de la clase CineTicketsNotFound son creados de manera satisfactoria."""
+    # Act
+    ct_exception = CineTicketsNotFound(argumento)
+
+    # Assert
+    assert_that(ct_exception).is_type_of(CineTicketsNotFound)
+    assert_that(ct_exception).is_instance_of(CineTicketsClientError)
+    assert_that(ct_exception.args[0]).is_equal_to(
+        f'CLIENT ERROR: Recurso no encontrado: '
+        f'{argumento if isinstance(argumento, str) else f"{type(argumento)} con atributos {argumento.__dict__}"}')

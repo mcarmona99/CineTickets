6143ca8fc6342207bce26ae3d8294f3f82a27602
Manuel Carmona Pérez <e.manuelvillar@go.ugr.es>
2021-11-12 20:54:46 +0100

    Rework de excepciones en favor de client y server errors

--- cinetickets/CineTicketsException.py
+++ cinetickets/CineTicketsException.py
@@ -1,12 +1,43 @@
-class CineTicketsException(Exception):
-    ERROR_CODES_DESCRIPTION = {
-        1: "No hay pelicula disponible asociada al nombre dado",
-        2: "El grupo no puede ser añadido a la distribución",
-    }
+class CineTicketsClientError(Exception):
+    """Clase que instancia las excepciones referentes a respuestas originadas por un error del cliente."""
 
-    MENSAJE_ERROR_NO_EXISTENTE = 'Sin mensaje para este código de error'
+    def __init__(self, msg: str):
+        """Constructor de la clase.
 
-    def __init__(self, codigo):
-        super().__init__()
-        self.codigo = codigo
-        self.mensaje = self.ERROR_CODES_DESCRIPTION.get(codigo, self.MENSAJE_ERROR_NO_EXISTENTE)
+        Parameters
+        ----------
+        msg : str
+            Mensaje de la excepción.
+        """
+        super().__init__(f'CLIENT ERROR: {msg}')
+
+
+class CineTicketsServerError(Exception):
+    """Clase que instancia las excepciones referentes a respuestas originadas por un error del servidor."""
+
+    def __init__(self, msg: str):
+        """Constructor de la clase.
+
+        Parameters
+        ----------
+        msg : str
+            Mensaje de la excepción.
+        """
+        super().__init__(f'SERVER ERROR: {msg}')
+
+
+class CineTicketsNotFound(CineTicketsClientError):
+    """Error referente a peticiones de recursos no encontrados."""
+
+    def __init__(self, recurso: object):
+        """Constructor de la clase.
+
+        Parameters
+        ----------
+        recurso : object
+            Recurso no encontrado.
+        """
+        super().__init__(
+            f'Recurso no encontrado: '
+            f'{recurso if isinstance(recurso, str) else f"{type(recurso)} con atributos {recurso.__dict__}"}')
+        self.code = 404
